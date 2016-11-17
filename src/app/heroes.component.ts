970244d40dcb85ac0aeaee397e56f007ca0f738a
Ernie <ernest.homsy@parsons.com>
2016-11-17 17:22:18 -0500

    forgot to add selected styling when hero is selected

--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -60,11 +60,12 @@ import {HeroService} from "./hero.service";
 
 
 <ul class="heroes">
-  <li *ngFor="let hero of heroes" (click)="onSelect(hero)">
+  <li *ngFor="let hero of heroes" (click)="onSelect(hero)" [class.selected]="hero === selectedHero">
     <span class="badge">{{hero.id}}</span> {{hero.name}}
   </li>
 </ul>
 <!--<my-hero-detail [hero]="selectedHero"></my-hero-detail>-->
+
 
 
 
