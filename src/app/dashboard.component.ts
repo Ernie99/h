2294c5a0491abc7364228165bb2d01edf63bcf85
Ernie <ernest.homsy@parsons.com>
2016-11-17 15:51:13 -0500

    populate the dashboard

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -1,7 +1,27 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {HeroService} from "./hero.service";
+import {Hero} from "./hero";
 
 @Component({
   selector: 'my-dashboard',
-  template: '<h3>My Dashboard</h3>'
+  template: `
+<h3>top heroes</h3>
+<div class="grid grid-pad">
+  <div *ngFor="let hero of heroes" class="col-1-4">
+    <div class="module hero">
+      <h4>{{hero.name}}</h4>
+    </div>
+  </div>
+
+</div>
+`
 })
-export class DashComponent {}
+export class DashComponent implements OnInit{
+  heroes: Hero[] = [];
+  constructor(private heroService: HeroService){
+  }
+  //
+  ngOnInit(): void {
+    this.heroService.getHeroes().then(heroes => (this.heroes = heroes.slice(1,5)));
+  }
+}
